9f2c7e40795c64c29c10a1bc683cf00f8bc1a53d
Ethan Yifan Xu <yxx218@case.edu>
2013-11-21 00:19:03 -0500

    Switched Fitting W and H

--- R/rnmf.R
+++ R/rnmf.R
@@ -29,7 +29,6 @@
 #' "fit": W \%*\% H.
 #' "trimmed1": A list of locations of trimmed cells after stage one in each iteration. (to be updated)
 #' "trimmed2": A list of locations of trimmed cells after stage two in each iteration. (to be updated)
-#' "SS": Sum of squares of the difference. (to be updated)
 #' "niter": Number of iterations performed.
 #'
 #' @keywords Non-negative matrix factorization, Robustness, Sparsity, Trimming
@@ -66,10 +65,10 @@ rnmf = function(A, k = 5, alpha = 0, beta = 0, maxit = 50, tol = 0.01,
     A.f = data.frame(value = as.vector(A), x = rep(1:p, n), y = rep(1:n, each = p), outs = FALSE)
     if(trim > 0){
         to.trim1 = vector("list", maxit)
-        to.trim2 = vector("list", maxit)
+        to.trim1 = vector("list", maxit)
     }else{
         to.trim1 = NULL
-        to.trim2 = NULL
+        to.trim1 = NULL
     }
     ## Initialization.
     ## Initialize W
@@ -97,7 +96,6 @@ rnmf = function(A, k = 5, alpha = 0, beta = 0, maxit = 50, tol = 0.01,
         if(nrow(ini.H) != k || ncol(ini.H) != n) {stop("ini.H has the wrong dimension!.")}
         H = ini.H
     }
-    SS = rep(NA,maxit) ## Initialize some of squares
     ## The following line creates a progress bar.
     pb <- txtProgressBar(min = 0, max = maxit, style = 3)
     obj = 1
@@ -142,10 +140,10 @@ rnmf = function(A, k = 5, alpha = 0, beta = 0, maxit = 50, tol = 0.01,
                 ## Find residuals
                 ## browser()
                 R = (A - W %*% H)^2
-                to.trim2[[i]] = which(rank(R) > round((1 - trim) * n * p))
+                to.trim1[[i]] = which(rank(R) > round((1 - trim) * n * p))
                 ## Update zeta
                 zeta = zeta.allTRUE
-                zeta[to.trim2[[i]]] = FALSE
+                zeta[to.trim1[[i]]] = FALSE
                 ## Refit W
                 W = Nnls.trimW(H, A, zeta, alpha, p1, n1, k, p)
                 J = nmlz(W) # Find the normalizing matrix of W
@@ -153,7 +151,7 @@ rnmf = function(A, k = 5, alpha = 0, beta = 0, maxit = 50, tol = 0.01,
             }
             obj[i] = l2((A - W %*% H)[zeta]) + l2(W) + sum(colSums(abs(H))^2)
             if(i > 1){
-                if(all(to.trim2[[i]] == to.trim2[[i-1]]) & sum((W - W.prev)^2)/sum(W.prev^2) < tol) break
+                if(all(to.trim1[[i]] == to.trim1[[i-1]]) & sum((W - W.prev)^2)/sum(W.prev^2) < tol) break
             }
             W.prev = W
         }
@@ -161,7 +159,71 @@ rnmf = function(A, k = 5, alpha = 0, beta = 0, maxit = 50, tol = 0.01,
         for(i in 1:maxit){
             flag = FALSE
             setTxtProgressBar(pb, i) # update the progress bar
-            ## Iteration stage 1. Estimate W. ||MW^T - C||.
+            ## Iteration stage 1. Estimate H. ||MH - C||
+            C = rbind(A, matrix(0,1,n))
+            M = rbind(W, sqrt(beta) * matrix(1, 1, k))
+            H = Nnls(M,C)
+            if(trim > 0){
+                R = (A - W %*% H)^2
+                if(variation == "col" | variation == "cellcol"){
+                    ## No change
+                }else if(variation == "cellrow" | variation == "cellall"){
+                    to.trim1[[i]] = which(rank(abs(R)) > round((1 - trim) * n * p))
+                    trim.row = unique(to.trim1[[i]] %% p)
+                    A.trim = A[-trim.row,]
+                    W.trim = W[-trim.row,]
+                    ## Fit H with trimmed A and W. 
+                    M.trim = rbind(W.trim, sqrt(beta) * matrix(1, nrow = 1, ncol = k))
+                    C.trim = rbind(A.trim, matrix(0, nrow = 1, ncol = n))
+                    H.prev <- H
+                    H = Nnls(M.trim,C.trim)
+                }else if(variation == "smooth"){
+                    to.trim1[[i]] = which(rank(abs(R)) > round((1 - trim) * n * p))
+                    A.s = matrix(smoothing2(A.f, to.trim1[[i]], p,n, frame = TRUE)$value,p,n)
+                    ## Fit H with trimmed A and W. 
+                    C.s = rbind(A.s, matrix(0, nrow = 1, ncol = n))
+                    H.prev <- H
+                    H = Nnls(M,C.s)
+                }else if(variation == "row" | variation == "all"){
+                    rsqr = apply(R, 1, l2) ## Find SS of residuals of rows
+                    to.trim1[[i]] = which(rank(rsqr) > round((1 - trim) * p))
+                    A.trim = A[-to.trim1[[i]],]
+                    W.trim = W[-to.trim1[[i]],]
+                    ## Fit H with trimmed A and W. 
+                    M.trim = rbind(W.trim, sqrt(beta) * matrix(1, nrow = 1, ncol = k))
+                    C.trim = rbind(A.trim, matrix(0, nrow = 1, ncol = n))
+                    H.prev <- H
+                    H = Nnls(M.trim,C.trim)
+                }else if(variation == "rowsmooth"){
+                    findmid = function(xvalue, datavalue){
+                        xrows = (0:(n-1)) * p + xvalue
+                        ## all data frame row numbers corresponding to row = xvalue.
+                        return(mean(datavalue[xrows], na.rm = TRUE))
+                    }
+                    R = (A - W %*% H)^2
+                    trims <- to.trim1[[i]] <- which(rank(abs(R)) > round((1 - trim) * n * p))
+                    ## locations of entries to be trimmed
+                    values = A.f$value ## All matrix values
+                    outs = rep(FALSE, n * p) ## Initialize outlier flags.
+                    outs[trims] = TRUE 
+                    values[trims] = NA ## Change outliers to missing values
+                    xs = A.f[trims,"x"] 
+                    uxs = unique(xs)
+                    moveto = sapply(uxs, findmid, values)
+                    values[trims] = mapvalues(xs, from = uxs, to = moveto)
+                    A.s = matrix(values,p,n)
+                    all.na = which(is.na(A.s[,1]))
+                    if(length(all.na) > 0) stop(paste("The following entire rows are trimmed:", all.na, ". I don't know how to fill the cells."))
+                    ## Fit H with trimmed A and W. 
+                    C.s = rbind(A.s, matrix(0, nrow = 1, ncol = n))
+                    H.prev <- H
+                    H = Nnls(M,C.s)
+                }else{
+                    stop("Wrong mode. Try one of the following: 'col', 'cellcol', 'rowrow', 'cellrow', 'smooth','all', 'cellall', 'rowsmooth'")
+                }
+            }
+
+            ## Iteration stage 2. Estimate W. ||MW^T - C||.
             C = rbind(t(A), matrix(0, nrow = k, ncol = p))
             M = rbind(t(H), sqrt(alpha) * diag(k))
             W = t(Nnls(M,C))
@@ -229,70 +291,6 @@ rnmf = function(A, k = 5, alpha = 0, beta = 0, maxit = 50, tol = 0.01,
                 }
             }
             W.prev <- W
-            ## Iteration stage 2. Estimate H. ||MH - C||
-            C = rbind(A, matrix(0,1,n))
-            M = rbind(W, sqrt(beta) * matrix(1, 1, k))
-            H = Nnls(M,C)
-            if(trim > 0){
-                R = (A - W %*% H)^2
-                if(variation == "col" | variation == "cellcol"){
-                    ## No change
-                }else if(variation == "cellrow" | variation == "cellall"){
-                    to.trim2[[i]] = which(rank(abs(R)) > round((1 - trim) * n * p))
-                    trim.row = unique(to.trim2[[i]] %% p)
-                    A.trim = A[-trim.row,]
-                    W.trim = W[-trim.row,]
-                    ## Fit H with trimmed A and W. 
-                    M.trim = rbind(W.trim, sqrt(beta) * matrix(1, nrow = 1, ncol = k))
-                    C.trim = rbind(A.trim, matrix(0, nrow = 1, ncol = n))
-                    H.prev <- H
-                    H = Nnls(M.trim,C.trim)
-                }else if(variation == "smooth"){
-                    to.trim2[[i]] = which(rank(abs(R)) > round((1 - trim) * n * p))
-                    A.s = matrix(smoothing2(A.f, to.trim1[[i]], p,n, frame = TRUE)$value,p,n)
-                    ## Fit H with trimmed A and W. 
-                    C.s = rbind(A.s, matrix(0, nrow = 1, ncol = n))
-                    H.prev <- H
-                    H = Nnls(M,C.s)
-                }else if(variation == "row" | variation == "all"){
-                    rsqr = apply(R, 1, l2) ## Find SS of residuals of rows
-                    to.trim2[[i]] = which(rank(rsqr) > round((1 - trim) * p))
-                    A.trim = A[-to.trim2[[i]],]
-                    W.trim = W[-to.trim2[[i]],]
-                    ## Fit H with trimmed A and W. 
-                    M.trim = rbind(W.trim, sqrt(beta) * matrix(1, nrow = 1, ncol = k))
-                    C.trim = rbind(A.trim, matrix(0, nrow = 1, ncol = n))
-                    H.prev <- H
-                    H = Nnls(M.trim,C.trim)
-                }else if(variation == "rowsmooth"){
-                    findmid = function(xvalue, datavalue){
-                        xrows = (0:(n-1)) * p + xvalue
-                        ## all data frame row numbers corresponding to row = xvalue.
-                        return(mean(datavalue[xrows], na.rm = TRUE))
-                    }
-                    R = (A - W %*% H)^2
-                    trims <- to.trim2[[i]] <- which(rank(abs(R)) > round((1 - trim) * n * p))
-                    ## locations of entries to be trimmed
-                    values = A.f$value ## All matrix values
-                    outs = rep(FALSE, n * p) ## Initialize outlier flags.
-                    outs[trims] = TRUE 
-                    values[trims] = NA ## Change outliers to missing values
-                    xs = A.f[trims,"x"] 
-                    uxs = unique(xs)
-                    moveto = sapply(uxs, findmid, values)
-                    values[trims] = mapvalues(xs, from = uxs, to = moveto)
-                    A.s = matrix(values,p,n)
-                    all.na = which(is.na(A.s[,1]))
-                    if(length(all.na) > 0) stop(paste("The following entire rows are trimmed:", all.na, ". I don't know how to fill the cells."))
-                    ## Fit H with trimmed A and W. 
-                    C.s = rbind(A.s, matrix(0, nrow = 1, ncol = n))
-                    H.prev <- H
-                    H = Nnls(M,C.s)
-                }else{
-                    stop("Wrong mode. Try one of the following: 'col', 'cellcol', 'rowrow', 'cellrow', 'smooth','all', 'cellall', 'rowsmooth'")
-                }
-            }
-            SS[i] = (sqrt(sum(A - W %*% H)^2))
         }
     }
     ## Close the link to the progress bar. 
@@ -322,6 +320,6 @@ rnmf = function(A, k = 5, alpha = 0, beta = 0, maxit = 50, tol = 0.01,
                 )
         }
     }
-    return(invisible(list(W = W, H = H, fit = fit, trimmed1 = to.trim1, trimmed2 = to.trim2, SS = SS, niter = i)))
+    return(invisible(list(W = W, H = H, fit = fit, trimmed1 = to.trim1, trimmed2 = to.trim1, niter = i)))
 }
 
